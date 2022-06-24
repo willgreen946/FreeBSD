cbbc8ef5353e7d9a49cb3bb342dbc838b2576a02
wrwrwrwrwr <72352225+willrollitt@users.noreply.github.com>
2022-06-24 09:55:12 +0100

    dwm and some rc's

---
+++ install.sh
@@ -0,0 +1,5 @@
+# Installing all packages I use
+exec pkg install doas vim vifm lynx git xorg neofetch feh mpv wifimgr bash 
+
+# Adding bash as default shell for my user
+chsh -s /usr/local/bin/bash will
